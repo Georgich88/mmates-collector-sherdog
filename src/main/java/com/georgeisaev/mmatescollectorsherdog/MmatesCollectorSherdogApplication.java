4dea736fb5d813c77ee44c57b3ae2876c454791e
Исаев Георгий Геннадьевич <isaev.georgii@otr.ru>
2021-11-04 18:00:41 +0300

    Initial commit

---
+++ src/main/java/com/georgeisaev/mmatescollectorsherdog/MmatesCollectorSherdogApplication.java
@@ -0,0 +1,13 @@
+package com.georgeisaev.mmatescollectorsherdog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmatesCollectorSherdogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MmatesCollectorSherdogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/georgeisaev/mmatescollectorsherdog/MmatesCollectorSherdogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.georgeisaev.mmatescollectorsherdog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmatesCollectorSherdogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
