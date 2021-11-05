6bc8dfb0dad29c32e5667b73512f1fce819f5349
Исаев Георгий Геннадьевич <isaev.georgii@otr.ru>
2021-11-05 16:42:26 +0300

    clean code

--- src/main/java/com/georgeisaev/mmatescollectorsherdog/config/SwaggerConfig.java
+++ src/main/java/com/georgeisaev/mmatescollectorsherdog/config/SwaggerConfig.java
@@ -1,7 +1,5 @@
 package com.georgeisaev.mmatescollectorsherdog.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
--- src/main/java/com/georgeisaev/mmatescollectorsherdog/data/dto/PromotionDto.java
+++ src/main/java/com/georgeisaev/mmatescollectorsherdog/data/dto/PromotionDto.java
@@ -6,7 +6,6 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
-import org.springframework.data.annotation.Id;
 
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Builder
