eecb71d5fe66170682b3a40d89c43a87da8e9dea
George Isaev <georgeisaev@gmail.com>
2022-09-04 16:06:33 +0400

    test: add tests for parsing fighters with wins, draws and ncs

--- src/test/java/com/georgeisaev/mmatescollectorsherdog/service/impl/FighterParserServiceImplIntegrationTest.java
+++ src/test/java/com/georgeisaev/mmatescollectorsherdog/service/impl/FighterParserServiceImplIntegrationTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.context.annotation.Import;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -19,11 +20,15 @@ class FighterParserServiceImplIntegrationTest {
 
   private static final String KHAMZAT_CHIMAEV_URL =
       "https://www.sherdog.com/fighter/Khamzat-Chimaev-280021";
+  private static final String TYRON_WOODLEY_URL =
+          "https://www.sherdog.com/fighter/Tyron-Woodley-42605";
 
+  private static final String FEDOR_EMELIANENKO_URL =
+          "https://www.sherdog.com/fighter/Fedor-Emelianenko-1500";
   @Autowired FighterParserService fighterParserService;
 
   @Test
-  @DisplayName("Should parse a valid given fighter ULR and produce a valid fighter")
+  @DisplayName("Should parse a fighter with wins")
   void shouldParseValidFighterUrl() {
 
     // GIVEN
@@ -35,5 +40,43 @@ class FighterParserServiceImplIntegrationTest {
 
     // THEN
     assertNotNull(khamzatChimaev);
+    assertNotNull(khamzatChimaev.getRecord());
+    assertThat(khamzatChimaev.getRecord().getWinsTotals()).isPositive();
+
   }
+
+  @Test
+  @DisplayName("Should parse a fighter with draws")
+  void shouldParseValidFighterWithDraws() {
+
+    // GIVEN
+    assertNotNull(fighterParserService);
+
+    // WHEN
+    final Fighter tyronWoodley =
+            assertDoesNotThrow(() -> fighterParserService.parse(TYRON_WOODLEY_URL));
+
+    // THEN
+    assertNotNull(tyronWoodley);
+    assertNotNull(tyronWoodley.getRecord());
+    assertThat(tyronWoodley.getRecord().getDraws()).isPositive();
+  }
+
+  @Test
+  @DisplayName("Should parse a fighter with N/C")
+  void shouldParseValidFighterWithNc() {
+
+    // GIVEN
+    assertNotNull(fighterParserService);
+
+    // WHEN
+    final Fighter fedorEmelianenko =
+            assertDoesNotThrow(() -> fighterParserService.parse(FEDOR_EMELIANENKO_URL));
+
+    // THEN
+    assertNotNull(fedorEmelianenko);
+    assertNotNull(fedorEmelianenko.getRecord());
+    assertThat(fedorEmelianenko.getRecord().getNc()).isPositive();
+  }
+
 }
